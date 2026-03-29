003fa91d0ddfd1870fd6122b3c4e3072f44f2dea
Vieri3 <egordohlyj4@gmail.com>
2026-03-29 13:57:12 +0300

    дебаг констант из-за особенностей vite

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,8 +3,11 @@ export const API_URL = import.meta.env.VITE_API_URL
 
 export const enum EDataSite {
     // ЭТО НАЗВАНИЕ Папки в которой юудет хранитьтся сайт
-    // это можэет быть http://my-name-site.com/parser-ve-vue/ index.html или  http://my-name-site.com/my-name-folder/
+    // это может быть http://my-name-site.com/parser-ve-vue/ index.html или  http://my-name-site.com/my-name-folder/
+    // пишем здесь но не применяем константу просто напоминание (после проверить чтобы в vite.config.ts в позиции base:'/parser-ve-vue/' )
+    // vite.config.ts не может считать с енама :))))
     SUBDIRECTORY_SITE_NAME = '/parser-ve-vue/',
+
    // это есть поддиректория в которой на сервере лежит наш бэкенд /api/parser/server.js или /api/parser/index.js или app.js или main.js 
     SUBDIRECTORY_SITE_API_NAME = '/api/parser/',
 
