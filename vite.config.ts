a523cb81b40e251535a3c6d4711cb4c8107b6e6d
Vieri3 <egordohlyj4@gmail.com>
2026-04-20 18:25:09 +0300

    смена домена смена настроек

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   // в моем основном сайте есть как бы папка с таким вот названием сайта vieri.xapp.one/parser-ve-vue/index.html вот типа такой путь должен быть
-  // и беем base из констант потому что так проще менять чтоб не лазить по всем папкам сайта
-  base: '/parser-ve-vue/',
+  // ппосле смены xapp.one/vieri/parser-ve-vue/index.html вот типа такой путь должен быть
+  //и меняем в env VITE_API_URL=https://xapp.one/vieri
+  // и берем base из констант потому что так проще менять чтоб не лазить по всем папкам сайта
+  base: '/vieri/parser-ve-vue/',
   plugins: [
     vue(),
     tailwindcss()
